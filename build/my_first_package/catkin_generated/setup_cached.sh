c067456bb076bdd094485dca014870a76f6f5f35
yiwei72 <wangyiwei9872@gmail.com>
2019-06-08 16:50:22 -0400

    initial ROS on TANK

---
+++ build/my_first_package/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ieisw/kaiROS/devel/.private/my_first_package:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ieisw/kaiROS/devel/.private/my_first_package/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ieisw/kaiROS/devel/.private/my_first_package/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/ieisw/kaiROS/build/my_first_package"
+export ROS_PACKAGE_PATH="/home/ieisw/kaiROS/src/my_first_package:$ROS_PACKAGE_PATH"
