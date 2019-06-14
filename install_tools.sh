91761ea73296cdeef58cd84a062c64e8fa1d0b82
Sumeet Kler <sumeetkler@gmail.com>
2019-06-13 21:56:23 -0700

    tesssssst

--- install_tools.sh
+++ install_tools.sh
@@ -1,2 +1,6 @@
+sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
+
+sudo apt-get install git
+git config core.hooksPath .githooks
 sudo apt install clang-format
--- src/my_first_package/src/hello_world.cpp
+++ src/my_first_package/src/hello_world.cpp
@@ -1,7 +1,8 @@
 #include "ros/ros.h"
 
 int main(int argc,
- char **argv) {
+ char **argv) 
+ {
   ros::init(argc, argv, "hello_world");
   ros::NodeHandle nh;
 
