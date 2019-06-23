309483c5e3170f9d3e27802fc91e2818e08bffb0
yiwei72 <wangyiwei9872@gmail.com>
2019-06-23 19:51:43 -0400

    cleaned up ros install process

--- install_tools.sh
+++ install_tools.sh
@@ -1,12 +1,12 @@
-#sudo add-apt-repository ppa:git-core/ppa
-#sudo apt-get update
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
 
-#sudo apt-get install git
-#git config core.hooksPath .githooks
-#sudo apt install clang-format
+sudo apt-get install git
+git config core.hooksPath .githooks
+sudo apt install clang-format
 
-#chmod +x .githooks/apply-format
-#chmod +x .githooks/pre-commit
+chmod +x .githooks/apply-format
+chmod +x .githooks/pre-commit
 
 toInstall=("ros-kinetic-desktop-full" "python-rosinstall" 
     "python-rosinstall-generator" "python-wstool" "build-essential" 
