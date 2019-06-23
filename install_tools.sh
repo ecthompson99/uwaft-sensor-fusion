9ab42fe8b23457c0ec8c371155265267079a58a1
yiwei72 <y2799wan@edu.uwaterloo.ca>
2019-06-23 18:24:16 -0400

    added if statements to check if software has been installed

--- install_tools.sh
+++ install_tools.sh
@@ -7,3 +7,23 @@ sudo apt install clang-format
 
 chmod +x .githooks/apply-format
 chmod +x .githooks/pre-commit
+
+toInstall=("ros-kinetic-desktop-full" "python-rosinstall" 
+    "python-rosinstall-generator" "python-wstool" "build-essential" 
+    "python-catkin-tools")
+
+notInstalled=()
+installed=()
+
+softwareCount="0"
+
+while [ $softwareCount -lt ${#toInstall[@]} ]
+do
+    if [ -n 'which' $toInstall(softwareCount)]; then
+        installed(softwareCount)=${toInstall(softwareCount)}
+    else
+        notInstalled(softwareCount)=${toInstall(softwareCount)}
+    fi
+softwareCount=$[$softwareCount+1]
+done
+
