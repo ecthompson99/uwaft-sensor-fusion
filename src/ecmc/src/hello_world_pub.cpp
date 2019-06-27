a39acc5fd3952692319f35ea7a5b5a2d2b8c08e3
julian <jmclower@edu.uwaterloo.ca>
2019-06-26 22:17:17 -0400

    start writing test

--- src/ecmc/src/hello_world_pub.cpp
+++ src/ecmc/src/hello_world_pub.cpp
@@ -1,17 +1,19 @@
 #include "ros/ros.h"
-#include "ecmc/GreetingMsg.h"
+#include "ecmc/RawSensorObjectData.h" // Where is this
  
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hello_world_pub");
     ros::NodeHandle nh;
       
-    ros::Publisher greeting_pub = nh.advertise<ecmc::GreetingMsg>("Greetings", 1000);  
+    ros::Publisher greeting_pub = nh.advertise<ecmc::RawSensorObjectData>("Greetings", 1000);  
   
-    ecmc::GreetingMsg msg;
-    msg.greeting = "Hello World!";
+    ecmc::RawSensorObjectData msg;
+    msg.accel_x[0] = 20;
+    msg.pos_y[0] = 20;
     int count = 1;
  
+    // With each iteration change some values of the message (what delays the loop speed)
     while(ros::ok()) {
         msg.num_greetings_sent = count;
         greeting_pub.publish(msg);
