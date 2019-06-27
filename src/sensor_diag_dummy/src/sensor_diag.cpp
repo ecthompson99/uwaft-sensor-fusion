1ffa8cd1df52c79cca40f718850a61eedbc72aec
yiwei72 <y2799wan@edu.uwaterloo.ca>
2019-06-27 17:14:55 -0400

    add sub

--- src/sensor_diag_dummy/src/sensor_diag.cpp
+++ src/sensor_diag_dummy/src/sensor_diag.cpp
@@ -1,15 +1,19 @@
-#include "ecmc/GreetingMsg.h"
+#include <stdio.h>
+#include <sstream>
+#include "sensor_diag_dummy/SensorDiagnosticDataMsg.h"
+//#include "sensor_diag_dummy/SensorDiagnosticFlagMsg.h"
 #include "ros/ros.h"
 
-void greetingCallback(const ecmc::GreetingMsg& msg) {
-  ROS_INFO_STREAM(msg.greeting << "\n"
-                               << "Greetings sent: " << msg.num_greetings_sent);
+void CANcallback(const sensor_diag_dummy::SensorDiagnosticFlagMsg& message) {
+  ROS_INFO_STREAM(<< "\n"
+                  << "The number is %d" << message.messageCounter-1);
 }
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "hello_world_sub");
+  ros::init(argc, argv, "sensor_diag");
   ros::NodeHandle nh;
-  ros::Subscriber greeting_sub = nh.subscribe("Greetings", 1000, greetingCallback);
+
+  ros::Subscriber sub = nh.subscribe("CANmsg", 1000, CANcallback);
 
   ros::spin();
 }
