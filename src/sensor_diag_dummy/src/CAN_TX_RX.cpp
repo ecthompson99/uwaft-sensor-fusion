c8102385d184242569ec69ef3682fe96df8b77f9
yiwei72 <y2799wan@edu.uwaterloo.ca>
2019-06-27 16:40:30 -0400

    modified to pub all required messages

--- src/sensor_diag_dummy/src/CAN_TX_RX.cpp
+++ src/sensor_diag_dummy/src/CAN_TX_RX.cpp
@@ -1,20 +1,53 @@
-#include "ecmc/GreetingMsg.h"
+#include <stdio.h>
+#include <sstream>
+#include "sensor_diag_dummy/SensorDiagnosticDataMsg.h"
 #include "ros/ros.h"
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "hello_world_pub");
+  ros::init(argc, argv, "CAN_TX_RX");
   ros::NodeHandle nh;
 
-  ros::Publisher greeting_pub = nh.advertise<ecmc::GreetingMsg>("Greetings", 1000);
+  ros::Publisher pub = nh.advertise<sensor_diag_dummy::SensorDiagnosticDataMsg>("CANmsg", 1000);
 
-  ecmc::GreetingMsg msg;
-  msg.greeting = "Hello World!";
-  int count = 1;
+  ros::Rate rate(1)
+
+  double starterConsistency, timeStamp, enderConsistency, counter,
+              checkSum, horizontalMisalign, absorbBlind, distortBlind;
+  uint16_t ITCinfo;
+  bool HWfail, SGUFail;
+  uint8_t messageCounter, messageCRC;
+
+  starterConsistency = 16.9856;
+  timeStamp = 12.4615212;
+  enderConsistency = 2.56876;
+  counter = 90.86517;
+  checkSum = 891.21252363;
+  horizontalMisalign = 1156.56448917;
+  absorbBlind = 7.7894138;
+  distortBlind = 16848918.1;
+
+  ITCinfo = 1;
+  
+  HWfail = 0;
+  SGUFail = 1;
+
+  messageCounter = 15;
+  messageCRC = 90;
 
   while (ros::ok()) {
-    msg.num_greetings_sent = count;
-    greeting_pub.publish(msg);
-    count++;
+    sensor_diag_dummy::SensorDiagnosticDataMsg message;
+
+    message.starterConsistency = starterConsistency;
+    message.timeStamp = timeStamp;
+    message.enderConsistency = enderConsistency;
+    message.counter = counter;
+    message.checkSum = checkSum;
+    message.horizontalMisalign = horizontalMisalign;
+    message.absorbBlind = absorbBlind;
+    message.distortBlind = distortBlind;
+
+    pub.publish(message);
+    rate.sleep();
     ros::spinOnce();
   }
 }
