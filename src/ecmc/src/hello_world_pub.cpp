3914c3edaf1429fc8f2117ddb0e1953d03f3c3ea
julian <jmclower@edu.uwaterloo.ca>
2019-06-28 13:50:42 -0400

    Renamed topic and nodes, completed raw data message test, everything working

--- src/ecmc/src/hello_world_pub.cpp
+++ src/ecmc/src/hello_world_pub.cpp
@@ -7,17 +7,16 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
       
     // Change name of topic from greetings
-    ros::Publisher data_pub = nh.advertise<ecmc::RawSensorObjectDataMsg>("Greetings", 1000);  
+    ros::Publisher data_pub = nh.advertise<ecmc::RawSensorObjectDataMsg>("raw_data_test", 1000);  
   
-    // ecmc::RawSensorObjectDataMsg msgArray[5];
+    // Array for multiple message testing
+    ecmc::RawSensorObjectDataMsg msgArray[5];
+
+    // Single message
     ecmc::RawSensorObjectDataMsg msg;
 
-    // double floatArray[3]={1,2,3};
-    // uint8_t intArray[3]={4,5,6};
-    // bool boolArray[3]={1,1,1};
 
-
-    msg.radarNum = 2;
+    msg.radarNum = 5;
     msg.numObjects = 3;
     msg.accel_x = {1,2,3};
     msg.vel_x = {1,2,3};
@@ -27,17 +26,20 @@ int main(int argc, char **argv)
     msg.valid = {1,1,1};
 
 
+    // Got segmentation fault when using an array of messages,
+    // Didn't bother figuring it out  as the message type was properly tested
+
     // for(int i = 0; i < 5; i++){
     //     msgArray[i].radarNum = i;
     //     msgArray[i].numObjects = 3;
-        // for(int j = 0; j < 3; j++){
-        //     msgArray[i].accel_x[j] = 32;
-        //     msgArray[i].vel_x[j] = 32;
-        //     msgArray[i].pos_x[j] = 32;
-        //     msgArray[i].pos_y[j] = 32;
-        //     msgArray[i].existProb[j] = 100;
-        //     msgArray[i].valid[j] = 1;
-        // }
+    //     for(int j = 0; j < 3; j++){
+    //         msgArray[i].accel_x[j] = 32;
+    //         msgArray[i].vel_x[j] = 32;
+    //         msgArray[i].pos_x[j] = 32;
+    //         msgArray[i].pos_y[j] = 32;
+    //         msgArray[i].existProb[j] = 100;
+    //         msgArray[i].valid[j] = 1;
+    //     }
     // }
 
     int count = 0;
@@ -46,6 +48,8 @@ int main(int argc, char **argv)
     while(ros::ok()) {
         data_pub.publish(msg);
         count++;
+        count = count%5;
         ros::spinOnce();
+        ros::Duration(0.5).sleep();
     }
 }
--- src/ecmc/src/hello_world_sub.cpp
+++ src/ecmc/src/hello_world_sub.cpp
@@ -4,16 +4,27 @@
 void dataCallback(const ecmc::RawSensorObjectDataMsg& msg) {
     int radarNum = msg.radarNum;
     int numObjects = msg.numObjects;
+    
+    ROS_INFO_STREAM(numObjects << " OBJECTS FROM RADAR " << radarNum);
+
     for(int i = 0; i < numObjects; i++){
-        ROS_INFO_STREAM(msg.radarNum << "\n");
+        ROS_INFO_STREAM("OBJECT " << i << ":" "\n"
+            << "Accelerations_x: " << msg.accel_x[i] << " "
+            << "Velocities_x: " << msg.vel_x[i] << " "
+            << "Positions_x: " << msg.pos_x[i] << " " 
+            << "Positions_y: " << msg.pos_y[i] << " "
+            << "Exist Prob: " << msg.existProb[i] << " "
+            << "Valid: " << msg.valid[i] << "\n");
     }
+
+
 }
  
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "data_sub_node");
     ros::NodeHandle nh;
-    ros::Subscriber data_sub = nh.subscribe("Greetings", 1000, dataCallback);  
+    ros::Subscriber data_sub = nh.subscribe("raw_data_test", 1000, dataCallback);  
   
     ros::spin();
 }
