422430dd4f866632d823f1e63deb0138077981e3
yiwei72 <y2799wan@edu.uwaterloo.ca>
2019-06-28 14:27:47 -0400

    test not execute ros

--- src/sensor_diag_dummy/src/master.cpp
+++ src/sensor_diag_dummy/src/master.cpp
@@ -3,16 +3,13 @@
 #include "sensor_diag_dummy/SensorDiagnosticFlagMsg.h"
 #include "ros.h"
 
-
 void diagCallback(const sensor_diag_dummy::SensorDiagnosticFlagMsg& radarMsg) {
   ROS_INFO_STREAM("\n"  << "Radar 1: %d " << radarMsg.radarReliability[0]
                   << "\n" << "Radar 2: %d " << radarMsg.radarReliability[1]
                   << "\n" << "Radar 3: %d " << radarMsg.radarReliability[2]
                   << "\n" << "Radar 4: %d " << radarMsg.radarReliability[3]
                   << "\n" << "Radar 5: %d " << radarMsg.radarReliability[4]
-                  << "\n" << "Radar 6: %d " << radarMsg.radarReliability[5]
-                  << "\n"
-  );
+                  << "\n" << "Radar 6: %d " << radarMsg.radarReliability[5]);
 }
 
 int main(int argc, char** argv) {
