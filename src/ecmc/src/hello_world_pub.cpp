404e14dfd7842e13259a9f78cb32cba4d5be88c0
julian <jmclower@edu.uwaterloo.ca>
2019-06-28 13:55:36 -0400

    Removed unused declarations

--- src/ecmc/src/hello_world_pub.cpp
+++ src/ecmc/src/hello_world_pub.cpp
@@ -8,13 +8,9 @@ int main(int argc, char **argv)
       
     // Change name of topic from greetings
     ros::Publisher data_pub = nh.advertise<ecmc::RawSensorObjectDataMsg>("raw_data_test", 1000);  
-  
-    // Array for multiple message testing
-    ecmc::RawSensorObjectDataMsg msgArray[5];
 
     // Single message
     ecmc::RawSensorObjectDataMsg msg;
-
 
     msg.radarNum = 5;
     msg.numObjects = 3;
@@ -24,23 +20,6 @@ int main(int argc, char **argv)
     msg.pos_y = {1,2,3};
     msg.existProb = {1,2,3};
     msg.valid = {1,1,1};
-
-
-    // Got segmentation fault when using an array of messages,
-    // Didn't bother figuring it out  as the message type was properly tested
-
-    // for(int i = 0; i < 5; i++){
-    //     msgArray[i].radarNum = i;
-    //     msgArray[i].numObjects = 3;
-    //     for(int j = 0; j < 3; j++){
-    //         msgArray[i].accel_x[j] = 32;
-    //         msgArray[i].vel_x[j] = 32;
-    //         msgArray[i].pos_x[j] = 32;
-    //         msgArray[i].pos_y[j] = 32;
-    //         msgArray[i].existProb[j] = 100;
-    //         msgArray[i].valid[j] = 1;
-    //     }
-    // }
 
     int count = 0;
  
