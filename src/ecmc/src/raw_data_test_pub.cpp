9f6aaf38a2be050cbe76f982d46a4c4793af54c4
Sumeet Kler <sumeetkler@gmail.com>
2019-09-19 00:51:23 -0400

    inital commit, code compiling needs to be tested with hardware

--- src/ecmc/src/raw_data_test_pub.cpp
+++ src/ecmc/src/raw_data_test_pub.cpp
@@ -1,7 +1,8 @@
 #include "ros/ros.h"
 
+#include <canlib.h>
 #include "ecmc/raw_sensor_object_data_msg.h"
- 
+
 static const uint32_t pub_buffer_size = 100;
 
 int main(int argc, char **argv)
@@ -22,10 +23,29 @@ int main(int argc, char **argv)
     msg.pos_y = {1,2,3};
     msg.exist_prob = {1,2,3};
     msg.valid = {1,1,1};
- 
+
+    canHandle hnd;
+    canInitializeLibrary();
+    hnd = canOpenChannel(0, canOPEN_EXCLUSIVE);
+    if (hnd < 0) {
+      char msg[64];
+      canGetErrorText((canStatus)hnd, msg, sizeof(msg));
+      fprintf(stderr, "canOpenChannel failed (%s)\n", msg);
+      exit(1);
+    }
+    canSetBusParams(hnd, canBITRATE_250K, 0, 0, 0, 0, 0);
+    canSetBusOutputControl(hnd, canDRIVER_NORMAL);
+    canBusOn(hnd);
+    char *can_msg = "HELLO!";
+
     while(ros::ok()) {
         test_data_pub.publish(msg);
+        canWrite(hnd, 123, (void *)can_msg, 6, 0);
+        canWriteSync(hnd, 500);
         ros::spinOnce();
         ros::Duration(0.5).sleep();
     }
+
+    canBusOff(hnd);
+    canClose(hnd);
 }
--- src/ecmc/src/raw_data_test_sub.cpp
+++ src/ecmc/src/raw_data_test_sub.cpp
@@ -1,31 +1,56 @@
 #include "ros/ros.h"
 
+#include <canlib.h>
 #include "ecmc/raw_sensor_object_data_msg.h"
- 
+
 static const int sub_buffer_size = 100;
 
 void raw_data_recv_callback(const ecmc::raw_sensor_object_data_msg& msg) {
-    uint8_t radar_num = msg.radar_num;
-    uint8_t num_objects = msg.num_objects;
+  uint8_t radar_num = msg.radar_num;
+  uint8_t num_objects = msg.num_objects;
 
-    ROS_INFO_STREAM(unsigned(num_objects) << " OBJECTS FROM RADAR " << unsigned(radar_num) << "\n");
+  ROS_INFO_STREAM(unsigned(num_objects) << " OBJECTS FROM RADAR " << unsigned(radar_num) << "\n");
 
-    for(uint32_t object_number = 0; object_number < num_objects; object_number++){
-        ROS_INFO_STREAM("OBJECT " << object_number << ":" "\n"
-            << "Accelerations_x: " << msg.accel_x[object_number] << " "
-            << "Velocities_x: " << msg.vel_x[object_number] << " "
-            << "Positions_x: " << msg.pos_x[object_number] << " "
-            << "Positions_y: " << msg.pos_y[object_number] << " "
-            << "Exist Prob: " << msg.exist_prob[object_number] << " "
-            << "Valid: " << msg.valid[object_number] << "\n");
-    }
+  for (uint32_t object_number = 0; object_number < num_objects; object_number++) {
+    ROS_INFO_STREAM("OBJECT " << object_number << ":"
+                                                  "\n"
+                              << "Accelerations_x: " << msg.accel_x[object_number] << " "
+                              << "Velocities_x: " << msg.vel_x[object_number] << " "
+                              << "Positions_x: " << msg.pos_x[object_number] << " "
+                              << "Positions_y: " << msg.pos_y[object_number] << " "
+                              << "Exist Prob: " << msg.exist_prob[object_number] << " "
+                              << "Valid: " << msg.valid[object_number] << "\n");
+  }
 }
- 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "data_sub_node");
-    ros::NodeHandle test_data_sub_nh;
-    ros::Subscriber test_data_sub = test_data_sub_nh.subscribe("raw_data_test", sub_buffer_size, raw_data_recv_callback);  
-  
-    ros::spin();
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "data_sub_node");
+  ros::NodeHandle test_data_sub_nh;
+  ros::Subscriber test_data_sub = test_data_sub_nh.subscribe("raw_data_test", sub_buffer_size, raw_data_recv_callback);
+
+  canHandle hnd;
+  canInitializeLibrary();
+  hnd = canOpenChannel(1, canOPEN_EXCLUSIVE);
+  if (hnd < 0) {
+    char msg[64];
+    canGetErrorText((canStatus)hnd, msg, sizeof(msg));
+    fprintf(stderr, "canOpenChannel failed (%s)\n", msg);
+    exit(1);
+  }
+  canSetBusParams(hnd, canBITRATE_250K, 0, 0, 0, 0, 0);
+  canSetBusOutputControl(hnd, canDRIVER_NORMAL);
+  canBusOn(hnd);
+  long id;
+  char msg[6] = {0};
+  unsigned int dlc;
+  unsigned int flag;
+  unsigned long time;
+  while (ros::ok()) {
+    canStatus status = canRead(hnd, &id, &msg, &dlc, &flag, &time);
+    if (canOK == status) {
+      ROS_INFO_STREAM(msg);
+    }
+    ros::spinOnce();
+    ros::Duration(0.5).sleep();
+  }
 }
