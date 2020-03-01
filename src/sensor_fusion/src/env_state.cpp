0fc5e99214a3c3362b2c040de97b443b702083a0
sophyli100 <sophyli100@gmail.com>
2020-03-01 15:51:01 -0500

    add test

--- src/sensor_fusion/src/env_state.cpp
+++ src/sensor_fusion/src/env_state.cpp
@@ -1,4 +1,5 @@
 #include "env_state.h"
+#include "object_state.h"
 
 EnvironmentState::EnvironmentState(ros::NodeHandle* node_handle) : env_state_node_handle(node_handle) {
   filtered_object_sub = env_state_node_handle->subscribe("filtered_object", MESSAGE_BUFFER_SIZE,
@@ -14,7 +15,28 @@ EnvironmentState::~EnvironmentState() {}
 void EnvironmentState::publish_object_output() { object_output_pub.publish(object_output_msg); }
 
 void EnvironmentState::filtered_object_callback(const sensor_fusion::filtered_object_msg& filtered_msg) {
- 
+    // TODO:
+    object_output_msg.obj_id = 1;
+    object_output_msg.obj_dx = 2.2;
+    object_output_msg.obj_lane = 1;
+    object_output_msg.obj_vx = 3.3;
+    object_output_msg.obj_dy = 5.6;
+    object_output_msg.obj_ax = 12.4;
+    object_output_msg.obj_path = 1;
+    object_output_msg.obj_vy = 16.7;
+    object_output_msg.obj_timestamp = 112;
+    
+    ROS_INFO_STREAM("\n"
+                    << "obj_id " << object_output_msg.obj_id << "\n"
+                    << "obj_dx " << object_output_msg.obj_dx << "\n"
+                    << "obj_lane " << object_output_msg.obj_lane << "\n"
+                    << "obj_vx " << object_output_msg.obj_vx << "\n"
+                    << "obj_dy" << object_output_msg.obj_dy << "\n"
+                    << "obj_ax " << object_output_msg.obj_ax << "\n"
+                    << "obj_path " << object_output_msg.obj_path << "\n"
+                    << "obj_vy" << object_output_msg.obj_vy << "\n"
+                    << "obj_timestamp " << object_output_msg.obj_timestamp << "\n");
+
 }
 
 
