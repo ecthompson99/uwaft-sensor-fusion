d5242325de9be7cd46e4679b41c670172f179738
sophyli100 <sophyli100@gmail.com>
2020-03-08 11:15:47 -0400

    fix linker error

--- src/sensor_fusion/src/object_state.cpp
+++ src/sensor_fusion/src/object_state.cpp
@@ -1,7 +1,10 @@
 #include "object_state.h"
 // #include "string.h"
 
-uint8_t ObjectState::get_obj_id() const { return this->obj_id;}
+ObjectState::ObjectState(){}
+ObjectState::~ObjectState() {}
+
+uint8_t ObjectState::get_obj_id() const { return obj_id;}
 
 void ObjectState::copy_info(const sensor_fusion::filtered_object_msg& filtered_msg) {
     // cout << "Copy constructor allocating ptr." << endl;
@@ -9,13 +12,13 @@ void ObjectState::copy_info(const sensor_fusion::filtered_object_msg& filtered_m
     // *ptr = *filtered_msg.ptr;
     
     
-    this->obj_id = filtered_msg.obj_id;
-    this->obj_dx = filtered_msg.obj_dx;
-    this->obj_lane = filtered_msg.obj_lane;
-    this->obj_vx = filtered_msg.obj_vx;
-    this->obj_dy = filtered_msg.obj_dy;
-    this->obj_ax = filtered_msg.obj_ax;
-    this->obj_path = filtered_msg.obj_path;
-    this->obj_vy = filtered_msg.obj_vy;
-    this->obj_timestamp = filtered_msg.obj_timestamp;
+    obj_id = filtered_msg.obj_id;
+    obj_dx = filtered_msg.obj_dx;
+    obj_lane = filtered_msg.obj_lane;
+    obj_vx = filtered_msg.obj_vx;
+    obj_dy = filtered_msg.obj_dy;
+    obj_ax = filtered_msg.obj_ax;
+    obj_path = filtered_msg.obj_path;
+    obj_vy = filtered_msg.obj_vy;
+    obj_timestamp = filtered_msg.obj_timestamp;
 }
