c707bfb52788dc454ee5259da22ab6ed7671adcf
Sophy Li <sophyli100@gmail.com>
2020-03-10 19:50:43 -0400

    populate test script

--- src/sensor_fusion/test/test_env_state.cpp
+++ src/sensor_fusion/test/test_env_state.cpp
@@ -1,8 +1,26 @@
-#include "env_state.cpp"
-
+#include "env_state.h"
 #include <gtest/gtesh.h>
 
-TEST()
+TEST(filtered_object_callback, validLogic){
+  
+
+}
+TEST(filtered_object_callback, validCallback){
+
+
+}
+
+TEST(add_object, validLogic){}
+TEST(update_object, validLogic){}
+TEST(check_timestamp, validLogic){}
+TEST(update_env_state, validLogic){}
+TEST(find_target_object, validLogic){}
+
+TEST(publish_obj_msg, validLogic){}
+TEST(publish_obj_msg, validCallback){}
+
+
+
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "env_state_test");
