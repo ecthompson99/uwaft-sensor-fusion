7bf74849225c972cfa67ad1b40d53f21ef6e877b
Sophy Li <sophyli100@gmail.com>
2020-03-13 18:21:50 -0400

    callback

--- src/sensor_fusion/test/test_env_state.cpp
+++ src/sensor_fusion/test/test_env_state.cpp
@@ -46,11 +46,67 @@ TEST(FiltObjCallback, validLogic){
 
 }
 
-TEST(FiltObjCallback, validCallback){}
+TEST(FiltObjCallback, validCallback){
+  ros::NodeHandle env_state_node_handle;
+  EnvironmentState env_state_test(&env_state_node_handle);
+
+  ros::NodeHandle filtered_object_node_handle;
+  ros::Publisher pub = filtered_object_node_handle.advertise<sensor_fusion::filtered_object_msg>(
+    "kf_dummy_data",  MESSAGE_BUFFER_SIZE);
+  )
+
+  sensor_fusion::object_output_msg object_output_expected;
+  object_output_expected.obj_id = 1;
+  object_output_expected.obj_dx = 12;
+  object_output_expected.obj_lane = 2;
+  object_output_expected.obj_vx = 23;
+  object_output_expected.obj_dy = 46;
+  object_output_expected.obj_ax = 45;
+  object_output_expected.obj_path = 1;
+  object_output_expected.obj_vy = 67;
+  object_output_expected.obj_timestamp = 134;
+  object_output_expected.object_track_num = 1;
+
+  sensor_fusion::filtered_object_msg filtered_msg;
+  filtered_msg.obj_id = 1;
+  filtered_msg.obj_dx = 12;
+  filtered_msg.obj_lane = 2;
+  filtered_msg.obj_vx = 23;
+  filtered_msg.obj_dy = 46;
+  filtered_msg.obj_ax = 45;
+  filtered_msg.obj_path = 1;
+  filtered_msg.obj_vy = 67;
+  filtered_msg.obj_timestamp = 134;
+
+  pub.publish(filtered_msg);
+  ros::spinOnce();
+
+  sensor_fusion::object_output_msg object_output = env_state_test.get_object_output_msg();
+
+  ASSERT_EQ(object_output_expected.obj_id, object_output.obj_id);
+  ASSERT_EQ(object_output_expected.obj_dx, object_output.obj_dx);
+  ASSERT_EQ(object_output_expected.obj_lane, object_output.obj_lane);
+  ASSERT_EQ(object_output_expected.obj_vx, object_output.obj_vx);
+  ASSERT_EQ(object_output_expected.obj_dy, object_output.obj_dy);
+  ASSERT_EQ(object_output_expected.obj_ax, object_output.obj_ax);
+  ASSERT_EQ(object_output_expected.obj_path, object_output.obj_path);
+  ASSERT_EQ(object_output_expected.obj_vy, object_output.obj_vy);
+  ASSERT_EQ(object_output_expected.obj_timestamp, object_output.obj_timestamp);
+  ASSERT_EQ(object_output_expected.object_track_num, object_output.object_track_num);
+
+}
 
 TEST(AddObject, validLogic){}
 TEST(UpdateObject, validLogic){}
-TEST(CheckTimestamp, validLogic){}
+TEST(CheckTimestamp, validLogic){
+  sensor_fusion::filtered_object_msg filtered_msg;
+  filtered_msg.obj_timestamp = 
+  sensor_fusion::object_output_msg object_output_expected;
+
+
+
+
+}
 TEST(UpdateEnvState, validLogic){}
 TEST(FindTargetObjects, validLogic){}
 
