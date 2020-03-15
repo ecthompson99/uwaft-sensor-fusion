b2725834a264e30a4468bf4400a619af1b711aa2
Sophy Li <sophyli100@gmail.com>
2020-03-15 13:34:07 -0400

    added update fcn

--- src/sensor_fusion/test/test_env_state.cpp
+++ src/sensor_fusion/test/test_env_state.cpp
@@ -1,21 +1,20 @@
-#include <gtest/gtesh.h>
+#include <gtest/gtest.h>
 #include "env_state.h"
 
 TEST(FiltObjCallback, validLogic){
   ros::NodeHandle env_state_node_handle;
   EnvironmentState env_state_test(&env_state_node_handle);
 
-  sensor_fusion::object_output_msg object_output_expected;
-  object_output_expected.obj_id = 1;
-  object_output_expected.obj_dx = 12;
-  object_output_expected.obj_lane = 2;
-  object_output_expected.obj_vx = 23;
-  object_output_expected.obj_dy = 46;
-  object_output_expected.obj_ax = 45;
-  object_output_expected.obj_path = 1;
-  object_output_expected.obj_vy = 67;
-  object_output_expected.obj_timestamp = 134;
-  object_output_expected.object_track_num = 1;
+  sensor_fusion::object_output_msg output_msg_expected;
+  output_msg_expected.obj_id = 1;
+  output_msg_expected.obj_dx = 12;
+  output_msg_expected.obj_lane = 2;
+  output_msg_expected.obj_vx = 23;
+  output_msg_expected.obj_dy = 46;
+  output_msg_expected.obj_ax = 45;
+  output_msg_expected.obj_path = 1;
+  output_msg_expected.obj_vy = 67;
+  output_msg_expected.obj_timestamp = 134;
 
   sensor_fusion::filtered_object_msg filtered_msg;
   filtered_msg.obj_id = 1;
@@ -30,20 +29,17 @@ TEST(FiltObjCallback, validLogic){
 
   env_state_test.filtered_object_callback(filtered_msg);
 
-  sensor_fusion::object_output_msg object_output = env_state_test.get_object_output_msg();
+  sensor_fusion::object_output_msg output_msg = env_state_test.get_object_output_msg();
 
-  ASSERT_EQ(object_output_expected.obj_id, object_output.obj_id);
-  ASSERT_EQ(object_output_expected.obj_dx, object_output.obj_dx);
-  ASSERT_EQ(object_output_expected.obj_lane, object_output.obj_lane);
-  ASSERT_EQ(object_output_expected.obj_vx, object_output.obj_vx);
-  ASSERT_EQ(object_output_expected.obj_dy, object_output.obj_dy);
-  ASSERT_EQ(object_output_expected.obj_ax, object_output.obj_ax);
-  ASSERT_EQ(object_output_expected.obj_path, object_output.obj_path);
-  ASSERT_EQ(object_output_expected.obj_vy, object_output.obj_vy);
-  ASSERT_EQ(object_output_expected.obj_timestamp, object_output.obj_timestamp);
-  ASSERT_EQ(object_output_expected.object_track_num, object_output.object_track_num);
-
-
+  ASSERT_EQ(output_msg_expected.obj_id, output_msg.obj_id);
+  ASSERT_EQ(output_msg_expected.obj_dx, output_msg.obj_dx);
+  ASSERT_EQ(output_msg_expected.obj_lane, output_msg.obj_lane);
+  ASSERT_EQ(output_msg_expected.obj_vx, output_msg.obj_vx);
+  ASSERT_EQ(output_msg_expected.obj_dy, output_msg.obj_dy);
+  ASSERT_EQ(output_msg_expected.obj_ax, output_msg.obj_ax);
+  ASSERT_EQ(output_msg_expected.obj_path, output_msg.obj_path);
+  ASSERT_EQ(output_msg_expected.obj_vy, output_msg.obj_vy);
+  ASSERT_EQ(output_msg_expected.obj_timestamp, output_msg.obj_timestamp);
 }
 
 TEST(FiltObjCallback, validCallback){}
@@ -112,9 +108,64 @@ TEST(AddObject, validLogic) {
 }
 
 TEST(UpdateObject, validLogic) {
+ros::NodeHandle env_state_node_handle;
+  EnvironmentState env_state_test(&env_state_node_handle);
 
+  ObjectState new_object_1;
+  new_object_1.obj_id = 4;
+  new_object_1.obj_dx = 10;
+  new_object_1.obj_dy = 15;
+  new_object_1.obj_lane = 1;
+  new_object_1.obj_vx = 4;
+  new_object_1.obj_vy = 6;
+  new_object_1.obj_path = 0;
+  new_object_1.obj_ax = 12;
+  new_object_1.obj_timestamp = 100;
+
+  ObjectState new_object_2;
+  new_object_1.obj_id = 5;
+  new_object_1.obj_dx = 15;
+  new_object_1.obj_dy = 6;
+  new_object_1.obj_lane = 2;
+  new_object_1.obj_vx = 43;
+  new_object_1.obj_vy = 64;
+  new_object_1.obj_path = 1;
+  new_object_1.obj_ax = 15;
+  new_object_1.obj_timestamp = 200;
+
+  std::vector<ObjectState> trackedObjects;
+  ObjectState tracked_object_1;
+  tracked_object_1.obj_id = 1;
+  tracked_object_1.obj_dx = 12;
+  tracked_object_1.obj_lane = 2;
+  tracked_object_1.obj_vx = 23;
+  tracked_object_1.obj_dy = 46;
+  tracked_object_1.obj_ax = 45;
+  tracked_object_1.obj_path = 1;
+  tracked_object_1.obj_vy = 67;
+  tracked_object_1.obj_timestamp = 134;
+
+  ObjectState tracked_object_2;
+  tracked_object_2.obj_id = 6;
+  tracked_object_2.obj_dx = 40;
+  tracked_object_2.obj_dy = 45;
+  tracked_object_2.obj_lane = 3;
+  tracked_object_2.obj_vx = 29;
+  tracked_object_2.obj_vy = 26;
+  tracked_object_2.obj_path = 0;
+  tracked_object_2.obj_ax = 21;
+  tracked_object_2.obj_timestamp = 300;
+
+  EnvironmentState::trackedObjects.push_back(tracked_object_1);
+  EnvironmentState::trackedObjects.push_back(tracked_object_2);
+
+  ASSERT_EQ(EnvironmentState::trackedObjects.size(), 2);
+  EnvironmentState::env_state_test.update_object(new_object_1, 0);
+  ASSERT_EQ(EnvironmentState::trackedObjects[0].obj_id, 4)
+  EnvironmentState::env_state_test.update_object(new_object_2, 1);
+  ASSERT_EQ(EnvironmentState::trackedObjects[1].obj_id, 5)
 }
-// 
+
 TEST(CheckTimestamp, validLogic){}
 TEST(UpdateEnvState, validLogic){}
 TEST(FindTargetObjects, validLogic){}
@@ -130,9 +181,9 @@ TEST(PublishOutputObj, validLogic){
   ros::Subscriber sub = output_obj_node_handle.subscribe("object_output", MESSAGE_BUFFER_SIZE, &test_output_obj_cb);
 
   env_state_test.publish_object_output();
-  ros::spinOnce(object_output_cb_called, false);
+  ros::spinOnce();
 
-  ASSERT_EQ(test_output_obj_cb, false);
+  ASSERT_EQ(object_output_cb_called, false);
 
 }
 
