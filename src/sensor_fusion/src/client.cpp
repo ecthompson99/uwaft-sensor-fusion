57bc9b4bb77e0c4cd3486617bdb9a0f80109e7b9
Maahir <maahir.gupta@gmail.com>
2020-03-20 01:44:05 -0400

    working service between temp  nodes

--- src/sensor_fusion/src/client.cpp
+++ src/sensor_fusion/src/client.cpp
@@ -1,23 +1,35 @@
 #include "ros/ros.h"
 #include "sensor_fusion/new_srv.h"
 #include <cstdlib>
+#include "sensor_fusion/dummy_msg.h"
    
 int main(int argc, char **argv){
     ros::init(argc, argv, "client");
-   
+    
+    void callback_temp(const sensor_fusion::dummy_msg& msg_obj);
+
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<sensor_fusion::new_srv>("service_topic");
+
+    ros::Subscriber sub = n.subscribe("tempTopic", 10, callback_temp);
+
     
     sensor_fusion::new_srv srv;
     
-    srv.request.temp = 1;
-
     if (client.call(srv)){
         double lmao = srv.response.data.apple;
-        std::cout << lmao; 
+        std::cout << lmao << srv.response.data.objstuff << std::endl;
     } else {
         ROS_ERROR("Failed to call service add_two_ints");
         return 1;
     }
+
+
+
+    ros::spinOnce();
     return 0;
+}
+
+void callback_temp(const sensor_fusion::dummy_msg& msg_obj){
+    std::cout << msg_obj.apple << std::endl;
 }
--- src/sensor_fusion/src/server.cpp
+++ src/sensor_fusion/src/server.cpp
@@ -1,16 +1,42 @@
 #include "ros/ros.h"
 #include "sensor_fusion/new_srv.h"
+#include "sensor_fusion/dummy_msg.h"
+
+    class tempClass{
+        public:
+        int rand;
+        std::string bruh;
+        
+        tempClass(int random, std::string rando) : rand(random), bruh(rando){}        
+    };
+
+    class container{
+        public:
+        std::vector<tempClass> vec;
+    };
 
     bool add(sensor_fusion::new_srv::Request &req, sensor_fusion::new_srv::Response &res){
-        
-        res.data.apple = 3;
-        
+        container cont;
+        tempClass obj(6, "heelo"); 
+        // cont.vec.push_back(obj);
+
+        res.data.apple = obj.rand;
+        res.data.objstuff = obj.bruh;
+        // res.data.objectContainer = container.vec;
+
         return true;
     }
 
     int main(int argc, char **argv) {
         ros::init(argc, argv, "server");
         ros::NodeHandle n;
+
+        ros::Publisher pub = n.advertise<sensor_fusion::dummy_msg>("tempTopic", 10);
+
+        // while(ros::ok()){
+        //     pub.publish(msgObj);
+        // }
+        
 
         ros::ServiceServer service = n.advertiseService("service_topic", add);
         
