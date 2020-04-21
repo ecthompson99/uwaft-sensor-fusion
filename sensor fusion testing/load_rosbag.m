e2a6cfd7d0f66de178ffa752dd346bb2fb145b9e
sophyli100 <sophyli100@gmail.com>
2020-04-21 14:10:20 -0600

    fixed target obj, adding tracked obj

--- sensor fusion testing/load_rosbag.m
+++ sensor fusion testing/load_rosbag.m
@@ -20,8 +20,8 @@ target_T = sortrows(struct2table(target_struct),3);
 tracked_T = sortrows(struct2table(tracked_struct),3);
 
 % Create array: ObjId,ObjDx,ObjLane,ObjVx,ObjDy,ObjAx,ObjPath,ObjVy,ObjTimestamp,ObjTrackNum 
-target_A = table2array(target_T(:,3:12));
-tracked_A = table2array(tracked_T(:,3:12));
+target_A = double(table2array(target_T(:,3:12)));
+tracked_A = double(table2array(tracked_T(:,3:12)));
 
 % Find unique objects & num of occurances
 [target_obj,ia,target_c] = unique(target_A(:,1),'rows');
@@ -43,9 +43,6 @@ targets = cell(size(target_obj,1),1);
 waypoints = cell(size(target_obj,1),1);
 speed_calc = cell(size(target_obj,1),1);
 
-% construct a straight road segment 50 m in length.
-road(scenario, [0 0 0; 50 0 0]);
-
 counter = 0; % used to keep track of unique object
 num_targets = double(target_count); % permanent array
 
@@ -54,8 +51,8 @@ for i = 1:size(target_obj,1)% loop through target_id
     % Specify rows in target_A and specify trajectory for unique object
     waypoints{i} = [target_A((counter + 1):(counter + num_targets(i)), 2) ...
         target_A((counter + 1):(counter + num_targets(i)), 5)]; % take dx and dy in columns 2 and 5 of array
-    speed_calc{i} = [sqrt(double(target_A((counter + 1):(counter + num_targets(i)), 4)).^2 + ...
-        double(target_A((counter + 1):(counter + num_targets(i)), 8).^2))]; % take vx and vy in columns 4 and 8 of array
+    speed_calc{i} = [sqrt(target_A((counter + 1):(counter + num_targets(i)), 4).^2 + ...
+        target_A((counter + 1):(counter + num_targets(i)), 8).^2)]; % take vx and vy in columns 4 and 8 of array
     
     counter = counter + num_targets(i);
 end
@@ -65,10 +62,10 @@ for i = 1:size(target_obj,1)% loop through target_id
 end
 
 % add a plot for debug
-plot(s,'Centerline','on','Waypoints','on','RoadCenters','on')
+plot(scenario,'Waypoints','on')
 
 % Start the simulation loop
-while advance(s)
+while advance(scenario)
  %fprintf('The vehicle is located at %.2f m at t=%.0f ms\n', targets{1}.Position(1), s.SimulationTime*1000)
  pause(0.1)
 end
@@ -76,6 +73,30 @@ end
 
 %% Driving scenario for tracked objects
 
+% create empty arrays for objects
+tracked_pos = cell(size(tracked_obj,1),1);
+tracked_vel = cell(size(tracked_obj,1),1);
 
+counter_1 = 0; % used to keep track of unique object
+num_tracked = double(tracked_count); % permanent array
 
+% Create bird's eye plot
+bep = birdsEyePlot('XLim',[0,90],'YLim',[-35,35]);
+objPlotter = detectionPlotter(bep,'DisplayName','Tracked Objects', ...
+    'MarkerEdgeColor','red', 'Marker','^');
+
+for i = 1:size(tracked_obj,1)% loop through target_id 
+    tracked_pos{i} = [tracked_A((counter_1 + 1):(counter_1 + num_tracked(i)), 2) ...
+        tracked_A((counter_1 + 1):(counter_1 + num_tracked(i)), 5)]; % take dx and dy in columns 2 and 5 of array
+    tracked_vel{i} = [tracked_A((counter_1 + 1):(counter_1 + num_tracked(i)), 4) ...
+        tracked_A((counter_1 + 1):(counter_1 + num_tracked(i)), 8)]; % take vx and vy in columns 4 and 8 of array
+    
+    counter_1 = counter_1 + num_tracked(i);
+    
+end
+
+% Plot Detections
+for i = 1:size(tracked_obj,1)% loop through target_id 
+    plotDetection(objPlotter, tracked_pos{i}, tracked_vel{i});
+end
 
