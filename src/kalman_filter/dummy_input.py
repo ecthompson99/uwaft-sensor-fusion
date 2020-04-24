72e914cecab5c0d36a1d54622b47c1a85aee3fee
Stephen Wang <ste53539@gmail.com>
2020-04-24 15:24:45 -0400

    new kf problem wip

--- src/kalman_filter/dummy_input.py
+++ src/kalman_filter/dummy_input.py
@@ -13,19 +13,24 @@ radar_sample.RadarDx, radar_sample.RadarDy, radar_sample.RadarVx, radar_sample.R
 radar_sample.RadarTimestamp = 0
 radar_sample.RadarDxSigma, radar_sample.RadarDySigma, radar_sample.RadarVxSigma, radar_sample.RadarAxSigma = .25, .25, .25, .25
 
-# me_sample = mobileye_object_data()
-# me_sample.MeDx, me_sample.MeDy, me_sample.MeVx, me_sample.MeTimestamp = 0,0,1,0
+me_sample = mobileye_object_data()
+me_sample.MeDx, me_sample.MeDy, me_sample.MeVx, me_sample.MeTimestamp = 0,0,1,0
+
+global_clk = 0
 
 while not rospy.core.is_shutdown():
     raw_input()
+    global_clk += .02
     radar_sample.RadarTimestamp += 1/50
     radar_sample.RadarDx = radar_sample.RadarTimestamp + gauss(0, .25)
     radar_sample.RadarDy = radar_sample.RadarTimestamp + gauss(0, .25)
     radar_sample.RadarVx, radar_sample.RadarVy = gauss(1, .25), gauss(1, .25)
     print(radar_sample)
     ra.publish(radar_sample)
-    # me_sample.MeDx += 1/15
-    # me_sample.MeDy += 1/15
-    # me_sample.MeTimestamp += 1/15
-    # print(me_sample)
-    # me.publish(me_sample)
+    if (me_sample.MeTimestamp + .04) <= global_clk:
+        raw_input()
+        me_sample.MeDx += 1/25 + gauss(0, .25)
+        me_sample.MeDy += 1/25 + gauss(0, .25)
+        me_sample.MeTimestamp += 1/25
+        print(me_sample)
+        me.publish(me_sample)
