26950d5c42a279c6941ea445ef6855ddb6d32ed6
Jillian <jcwong326@gmail.com>
2020-04-28 16:23:51 -0700

    fix indexing and access errors

--- sensor fusion testing/step5_binary_classification.m
+++ sensor fusion testing/step5_binary_classification.m
@@ -10,7 +10,7 @@ temp = load(file_path);
 ground_truth = temp.results;
 
 % Load sensor fusion output
-bag_path = strcat(pwd, '\2020-04-27-00-44-09.bag');
+bag_path = strcat(pwd, '\2020-04-27-14-38-09.bag');
 bag = rosbag(bag_path);
 temp = select(bag, 'Topic', '/binary_class');
 output = readMessages(temp, 'DataFormat', 'struct');
@@ -27,25 +27,25 @@ THRESHOLD = 11;
 
 % remember to change that 9000 (number of frames)
 % Go through every timestep
-for i = 1:300
+for i = 1:500
 
     % Loop through ground truth objects
-    for a = 1:size(ground_truth(i).Objects) 
+    for a = 1:size(ground_truth(i).Num_Objects) 
 
-        sf_flags = false(size(output{i,1}.Dx));
-        gt_flags = false(size(ground_truth(i).Objects));
+        sf_flags = false(size(output{i}.Dx,1),1);
+        gt_flags = false(ground_truth(i).Num_Objects,1);
 
         gt_object = ground_truth(i).Objects(1,a);
 
         % Loop through sensor fusion output objects
-        for b = 1:size(output{i,1}.Dx,1) 
-            sf_vector = [output{i,1}.Dx(b,1), output{i,1}.Dy(b,1)];
+        for b = 1:size(output{i}.Dx,1) 
+            sf_vector = [output{i}.Dx(b,1); output{i,1}.Dy(b,1)];
 
             % Check distance between vectors
             if norm(sf_vector - gt_object.Measurement) < THRESHOLD
                 % True Positives = number of objects detected by both outputs
                 sf_flags(b,1) = true;
-                gt_flags(b,1) = true;
+                gt_flags(a,1) = true;
                 TP = TP + 1;
                 total = total + 1;
                 break;
