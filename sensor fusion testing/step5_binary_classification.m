1ea879768ac0768640fa95fb2b2e8b578e14e621
Jillian <jcwong326@gmail.com>
2020-04-28 00:03:26 -0700

    fix index errors

--- sensor fusion testing/step5_binary_classification.m
+++ sensor fusion testing/step5_binary_classification.m
@@ -28,18 +28,20 @@ for i = 1:9000
     % Loop through ground truth objects
     for a = 1:size(ground_truth(i).Objects) 
 
+        sf_flags = false(size(output{i}.Dx));
+        gt_flags = false(size(ground_truth(i).Objects));
+
+        gt_object = ground_truth(i).Objects(1,a);
+
         % Loop through sensor fusion output objects
         for b = 1:size(output{i,1}.Dx,1) 
-            gt_object = ground_truth(i).Objects(1,a);
             sf_vector = [output{i,1}.Dx(b,1), output{i,1}.Dy(b,1)];
 
             % Check distance between vectors
             if norm(sf_vector - gt_object.Measurement) < THRESHOLD
-                % Remove matched object from both matrices
-                ground_truth(i).Objects(:,a) = [];
-                output{i}.Dx(b) = [];
-
                 % True Positives = number of objects detected by both outputs
+                sf_flags(b,1) = true;
+                gt_flags(b,1) = true;
                 TP = TP + 1;
                 total = total + 1;
                 break;
@@ -47,14 +49,12 @@ for i = 1:9000
         end
 
         % False Negatives = number of objects left in ground truth
-        for b = 1:size(ground_truth(i).Objects)
-            FN = FN + 1;
-            total = total + 1;
-        end
+        FN = FN + sum(gt_flags == false);
+        total = total + sum(gt_flags == false);
 
         % False Positives = number of objects left in sensor fusion algorithm
-        FP = FP + size(output{i,1}.Dx,1);
-        total = total + size(output{i,1}.Dx,1);
+        FP = FP + sum(sf_flags == false);
+        total = total + sum(sf_flags == false);
     end
 end
 
