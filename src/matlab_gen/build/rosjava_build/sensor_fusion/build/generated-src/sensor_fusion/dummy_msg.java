776a55aef9ddef2308d9198b726da6bfccefc3bd
Stephen Wang <ste53539@gmail.com>
2020-05-10 14:19:36 -0400

    removed matlab_gen and SF_visualization

--- src/matlab_gen/build/rosjava_build/sensor_fusion/build/generated-src/sensor_fusion/dummy_msg.java
+++
@@ -1,8 +0,0 @@
-package sensor_fusion;
-
-public interface dummy_msg extends org.ros.internal.message.Message {
-  static final java.lang.String _TYPE = "sensor_fusion/dummy_msg";
-  static final java.lang.String _DEFINITION = "\n\n";
-  static final boolean _IS_SERVICE = false;
-  static final boolean _IS_ACTION = false;
-}
