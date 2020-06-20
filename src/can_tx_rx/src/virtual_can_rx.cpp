7498a8229b95bfbaf18fda05490c0d5e9961d228
joanna diao <joannadiao@hotmail.com>
2020-06-20 13:57:47 -0400

    virtual can

---
+++ src/can_tx_rx/src/virtual_can_rx.cpp
@@ -0,0 +1,60 @@
+#include "ros/ros.h"
+#include <canlib.h>
+#include "can_tx_rx/ext_log_data.c"
+#include "can_tx_rx/ext_log_data.h"
+
+static const int sub_buffer_size = 100;
+
+int main(int argc, char** argv) {
+
+  ros::init(argc, argv, "data_sub_node");
+  ros::NodeHandle test_data_sub_nh;
+
+  canHandle hnd;
+  canInitializeLibrary();
+  hnd = canOpenChannel(1, canOPEN_EXCLUSIVE);
+  if (hnd < 0) {
+    char msg[64];
+    canGetErrorText((canStatus)hnd, msg, sizeof(msg));
+    fprintf(stderr, "canOpenChannel failed (%s)\n", msg);
+    exit(1);
+  }
+  canSetBusParams(hnd, canBITRATE_250K, 0, 0, 0, 0, 0);
+  canSetBusOutputControl(hnd, canDRIVER_NORMAL);
+  canBusOn(hnd);
+
+long id;
+uint8_t can_msg[8] = {0};
+unsigned int dlc, flags;
+unsigned long timestamp;
+//size_t size = 8;
+
+//while (ros::ok())
+//{
+    canStatus stat = canRead(hnd, &id, can_msg, &dlc, &flags, &timestamp);
+    if (stat != canERR_NOMSG) {
+        std::cout << "Failed, status == " << stat << std::endl;
+    }
+//}
+
+struct ext_log_data_obstacle_data_a_t a;
+struct ext_log_data_obstacle_data_a_t *frame_a = &a;
+
+int unpack_return = ext_log_data_obstacle_data_a_unpack(frame_a, can_msg, sizeof(can_msg));
+
+// std::cout << ext_log_data_obstacle_data_a_obstacle_pos_x_decode(a.obstacle_pos_x) << std::endl;
+// std::cout << ext_log_data_obstacle_data_a_obstacle_pos_y_decode(a.obstacle_pos_y) << std::endl;
+// std::cout << ext_log_data_obstacle_data_a_obstacle_vel_x_decode(a.obstacle_vel_x) << std::endl;
+// std:: cout << frame_a->obstacle_pos_x << std::endl;
+std::cout << a.obstacle_pos_x << std::endl;
+std::cout << a.obstacle_pos_y << std::endl;
+std::cout << a.obstacle_vel_x << std::endl
+
+canBusOff(hnd);
+canClose(hnd);
+
+ros:: spinOnce();
+
+return 0;
+
+}
--- src/can_tx_rx/src/virtual_can_tx.cpp
+++ src/can_tx_rx/src/virtual_can_tx.cpp
@@ -18,10 +18,14 @@ int main(int argc, char **argv)
     a.obstacle_pos_x = 240;
     a.obstacle_pos_y = 31;
     a.obstacle_vel_x = 100;
+    std::cout << a.obstacle_pos_x << std::endl;
 
     a.obstacle_pos_x = ext_log_data_obstacle_data_a_obstacle_pos_x_encode(a.obstacle_pos_x);
     a.obstacle_pos_y = ext_log_data_obstacle_data_a_obstacle_pos_y_encode(a.obstacle_pos_y);
     a.obstacle_vel_x = ext_log_data_obstacle_data_a_obstacle_vel_x_encode(a.obstacle_vel_x);
+    std::cout << a.obstacle_pos_x << std::endl;
+    std::cout << a.obstacle_pos_y << std::endl;
+    std::cout << a.obstacle_vel_x << std::endl;
 
     const struct ext_log_data_obstacle_data_a_t *frame_a = &a;
 
@@ -41,6 +45,7 @@ int main(int argc, char **argv)
     uint8_t can_msg[8] = {0};
     size_t size = 8u;
     int pack_return = ext_log_data_obstacle_data_a_pack(can_msg, frame_a, size);
+    
     
     while (ros::ok()) 
     {
