119407af9c3fd196b3980f916dcaf75d7b76bc3d
Jillian Wong <jcwong326@gmail.com>
2020-07-07 01:34:39 -0400

    change ground truth to fit simulation data format

--- sensor fusion testing/visualization_overlay/obj_plot.m
+++ sensor fusion testing/visualization_overlay/obj_plot.m
@@ -1,24 +1,27 @@
 %% Plot objects in video
-load('ground_truth/FTR_ground_truth.mat');
-load('sensor_fusion/sf_output.mat');
+load('simulation_gt.mat'); %ground_truth
+load('sf_output.mat'); %sf_results
 set(0,'DefaultFigureVisible','off');
 
-newVid = VideoWriter('tracked_objects.avi');
-newVid.FrameRate = 1;
+newVid = VideoWriter('tracked_objects_fr100.avi');
+newVid.FrameRate = 100;
 newVid.Quality = 100;
 open(newVid);
 
-for i = 10:10:3090%size(results,2) % sample plot for now (start:iteration:end)
+gt_offset = 6;
 
-    bep = birdsEyePlot('XLim',[0,100],'YLim',[-5,10]);
+for i = 7:size(sf_results,2) % sample plot for now (start:iteration:end)
+
+    bep = birdsEyePlot('XLim',[0,150],'YLim',[-15,15]);
     blazerPlotter = detectionPlotter(bep,'MarkerEdgeColor','black','DisplayName','Ground Truth Objects', 'Marker', 'o');
     sfPlotter = detectionPlotter(bep,'MarkerEdgeColor','red','DisplayName','Sensor Fusion Objects', 'Marker', '+');
 
-    blazer_positions = zeros(results(i).Num_Objects,2);
+    blazer_num_obj = size(ground_truth(i+gt_offset).Objects,1);
+    blazer_positions = zeros(blazer_num_obj);
     sf_positions = zeros(sf_results(i).Num_Objects,2);
 
-    for j = 1:results(i).Num_Objects
-        blazer_positions(j,1:2) = results(i).Objects(1,j).Measurement(:,1);
+    for j = 1:blazer_num_obj
+        blazer_positions(j,1:2) = ground_truth(i+gt_offset).Objects(j,:);
     end
     
     for j = 1:sf_results(i).Num_Objects
@@ -28,7 +31,7 @@ for i = 10:10:3090%size(results,2) % sample plot for now (start:iteration:end)
     plotDetection(blazerPlotter, blazer_positions);
     plotDetection(sfPlotter, sf_positions);
 
-    title(gca, char(num2str(results(i).Time + " seconds"))); 
+    title(gca, char(num2str(sf_results(i).Time + " seconds"))); 
     grid on;
     
     frame = getframe(gcf);
