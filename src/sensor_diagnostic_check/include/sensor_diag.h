52c5c88fe2f828035cfbf491ced1eccebc524d3d
Maahir Gupta <43038189+MaahirG@users.noreply.github.com>
2020-07-26 20:39:20 -0400

    added h file for diagnostics node

---
+++ src/sensor_diagnostic_check/include/sensor_diag.h
@@ -0,0 +1,40 @@
+#ifndef __SENSOR_DIAG_H__
+#define __SENSOR_DIAG_H__
+
+#include <stdio.h>
+#include <sstream>
+#include "ros/ros.h"
+#include <cstdlib>
+// subscriber includes
+#include "common/sensor_diagnostic_data_msg.h"  // will be unpacked can messages - emulating CANRXTX Node
+#include "common/sensor_diagnostic_flag_msg.h"  // will be unpacked can messages - emulating CANRXTX Node
+
+// srv files
+#include "common/sensor_diagnostic_flag_CH2.h"  
+#include "common/sensor_diagnostic_flag_CH3.h"  
+#include "common/sensor_diagnostic_flag_CH4.h"
+
+#define TX_RX_MESSAGE_BUFFER_SIZE 1000
+
+class SensorDiagnostics {
+    public: 
+        SensorDiagnostics(ros::NodeHandle* diag_nodehandle);    //constructor - pointer handle
+    private: 
+        ros::NodeHandle* diag_nodehandle;
+        ros::ServiceClient client_ch2; //
+        ros::ServiceClient client_ch3;
+        ros::ServiceClient client_ch4;
+
+        ros::Subscriber sub_CAN_data;
+        ros::Subscriber sub_CAN_flag;
+
+        void sub_CAN_data_callback(const common::sensor_diagnostic_data_msg& diag_data_msg);
+        void sub_CAN_flag_callback(const common::sensor_diagnostic_flag_msg& diag_flag_msg);
+
+}
+
+
+// sub to cantxrx dummy node (later a function)
+// srv request to mastertask when something is wrong - look in common for srv files
+
+#endif
