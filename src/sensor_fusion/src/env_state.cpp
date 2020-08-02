8a4c766b114a8d8c98c82ec172d4172810b0aff2
Sophy Li <sophyli100@gmail.com>
2020-08-02 18:31:35 -0400

    testing default msg

--- src/sensor_fusion/src/env_state.cpp
+++ src/sensor_fusion/src/env_state.cpp
@@ -281,21 +281,21 @@ bool EnvironmentState::env_state_srv_callback(sensor_fusion::env_state_srv::Requ
         return true;
 }
 
-// int main(int argc, char** argv){
-//     ros::init(argc, argv, "env_state");
-//     ros::NodeHandle env_state_node_handle;
-//     EnvironmentState env_state = EnvironmentState(&env_state_node_handle);
+int main(int argc, char** argv){
+    ros::init(argc, argv, "env_state");
+    ros::NodeHandle env_state_node_handle;
+    EnvironmentState env_state = EnvironmentState(&env_state_node_handle);
    
-//     // while (ros::ok()) {
-// 	// 	// env_state.publish_target_obj();
-// 	// 	env_state.publish_tracked_obj();
-// 	// 	ros::Rate(10).sleep();
-// 	// 	ros::spinOnce();
-//     // }
-// 	ros::spin();
+    // while (ros::ok()) {
+	// 	// env_state.publish_target_obj();
+	// 	env_state.publish_tracked_obj();
+	// 	ros::Rate(10).sleep();
+	// 	ros::spinOnce();
+    // }
+	ros::spin();
 
-// 	return 0;
-// }
+	return 0;
+}
 
 
 
