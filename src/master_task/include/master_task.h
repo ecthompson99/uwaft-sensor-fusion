bc9453e22ecee15f2df1d3ce8da87c4c0399d5d9
JoannaDiao <45150393+JoannaDiao@users.noreply.github.com>
2020-08-17 10:58:41 -0400

    Uwaft 2545 master task unit test (#56)
    * added functinos for master task requirements
    
    * implemented logic for ACC AEB and LCC requirements
    
    * developed master task requirement logics, need to unit test now
    
    * fixed errors so that code compiles
    
    * fixed minors issues for cav_1.5
    
    * unit testing modifications
    
    * added cout's to organize results
    
    * master task unit test completed with the exception of ACC-1.1
    
    * updated requirements based on requiremtn list
    
    * updated requirements based on requiremtn list
    
    * finished modifying logic based on updated requirement list, and finished unit testing

--- src/master_task/include/master_task.h
+++ src/master_task/include/master_task.h
@@ -11,7 +11,8 @@
 #include "common/acc_output_msg.h"
 #include "common/aeb_output_msg.h"
 #include "common/lcc_output_msg.h"
-#include "std_srvs/Empty.h"
+#include <thread>
+#include <time.h>
 
 static const uint8_t MASTER_MESSAGE_BUFFER_SIZE = 10;
 
@@ -29,6 +30,25 @@ class MasterTask {
   bool sensor_diagnostic_callback_CH2(common::sensor_diagnostic_flag_CH2::Request &req_CH2, common::sensor_diagnostic_flag_CH2::Response &res_CH2);
   bool sensor_diagnostic_callback_CH3(common::sensor_diagnostic_flag_CH3::Request &req_CH3, common::sensor_diagnostic_flag_CH3::Response &res_CH3);
   bool sensor_diagnostic_callback_CH4(common::sensor_diagnostic_flag_CH4::Request &req_CH4, common::sensor_diagnostic_flag_CH4::Response &res_CH4);
+  void CAV_1_5();
+  void CAV_1_6();
+  void ACC_1_1();
+  void ACC_15();
+  void ACC_17();
+  void ACC_20();
+  void ACC_4();
+  void AEB_13();
+  void AEB_17();
+  void AEB_22();
+  void AEB_23();
+  void AEB_24();
+  void AEB_26();
+  void AEB_14();
+  void CAV_2_2();
+  void LCC_1();
+  void LCC_10();
+  void LCC_11();
+  void LCC_3();
   common::can_comms_data_msg get_can_comms_msg();
 
  protected:
@@ -43,6 +63,62 @@ class MasterTask {
   ros::ServiceServer sensor_diagnostic_CH3_server;
   ros::ServiceServer sensor_diagnostic_CH4_server;
   common::can_comms_data_msg can_comms_msg;
+
+  bool ACC_ACTIVATION;
+  bool AEB_ACTIVATION;
+  bool LCC_ACTIVATION;
+
+  bool ACC_ALLOWED;
+  bool AEB_ALLOWED;
+  bool LCC_ALLOWED;
+
+  bool MABx_alive;
+  bool Jetson_alive;
+
+  double acc_speed_set_point;
+  int acc_gap_level;
+  double VEHICLE_SPEED;
+  double STEERING_ANGLE;
+  //acc topic 
+  bool ACC_FAULT;
+  double ACC_ACCEL;
+  //aeb topic
+  bool AEB_FAULT;
+  bool AEB_ENGAGED;
+  double AEB_ACCEL;
+  //lcc topic
+  bool LCC_FAULT;
+  double LCC_STEER;
+  //sensor_diagnostic_flag_CH2
+  bool FRONT_RADAR; //change to zero after unit testing!!
+  //sensor_diagnostic_flag_CH3
+  bool LEFT_CORNER_RADAR;
+  bool RIGHT_CORNER_RADAR;
+  //sensor_diagnostic_flag_CH4
+  bool MOBILEYE;
+
+  int buffer_time = 0.200; //ACC_1_1
+
+  bool initial_OFF_request = true;
+  bool initial_OFF_request_2 = true;
+  bool initial_OFF_request_3 = true;
+  bool initial_OFF_request_4 = true;
+  bool initial_OFF_request_5 = true;
+  bool initial_OFF_request_6 = true;
+  bool initial_OFF_request_7 = true;
+
+  ros::WallTime begin1;
+  ros::WallTime begin3;
+  ros::WallTime begin4;
+  ros::WallTime begin5;
+  ros::WallTime begin6;
+  ros::WallTime begin7;
+  ros::WallTime begin8;
+
+  double prev_vel; //ego vehicle previous velocity, used to calculate acceleration of ego vehicle
+  double curr_vel; //ego vehicle current velocity
+  double ego_accel;
+  ros::WallTime prev_time;
 };
 
 #endif  // __MASTER_TASK_H__
--- src/master_task/src/master_task.cpp
+++ src/master_task/src/master_task.cpp
@@ -25,62 +25,244 @@ void MasterTask::publish_can_comms_msg() { master_task_pub.publish(can_comms_msg
 
 void MasterTask::drive_ctrl_msg_callback(const common::drive_ctrl_input_msg& drive_ctrl_msg) 
 {
+    ACC_ACTIVATION = drive_ctrl_msg.acc_activation;
+    AEB_ACTIVATION = drive_ctrl_msg.aeb_activation;
+    LCC_ACTIVATION = drive_ctrl_msg.lcc_activation;
 
-  ROS_INFO_STREAM("\n"
-                  << "ACC allowed? " << unsigned(drive_ctrl_msg.acc_allowed) << "\n"
-                  << "AEB allowed? " << unsigned(drive_ctrl_msg.aeb_allowed) << "\n"
-                  << "LC allowed? " << unsigned(drive_ctrl_msg.lcc_allowed) << "\n"
-                  << "ACC speed set point " << drive_ctrl_msg.acc_speed_set_point << "\n"
-                  << "Vehicle speed " << drive_ctrl_msg.veh_spd << "\n");
+    ACC_ALLOWED = drive_ctrl_msg.acc_allowed;
+    AEB_ALLOWED = drive_ctrl_msg.aeb_allowed;
+    LCC_ALLOWED = drive_ctrl_msg.lcc_allowed;
+
+    MABx_alive = drive_ctrl_msg.MABx_alive;
+    Jetson_alive = drive_ctrl_msg.Jetson_alive;
+
+    acc_speed_set_point = drive_ctrl_msg.acc_speed_set_point;
+    acc_gap_level = drive_ctrl_msg.acc_gap_level;
+    VEHICLE_SPEED = drive_ctrl_msg.veh_spd;
+    STEERING_ANGLE = drive_ctrl_msg.str_ang;
 }
 
 void MasterTask::acc_output_msg_callback(const common::acc_output_msg& acc_msg)
 {
-    ROS_INFO_STREAM("\n"
-                  << "ACC fault? " << unsigned(acc_msg.acc_fault) << "\n"
-                  << "ACC acceleration is " << acc_msg.acc_accel << "\n");
+    ACC_FAULT = acc_msg.acc_fault;
+    ACC_ACCEL = acc_msg.acc_accel;
+    can_comms_msg.acc_fault = acc_msg.acc_fault;
 }
 
 void MasterTask::aeb_output_msg_callback(const common::aeb_output_msg& aeb_msg)
 {
-    ROS_INFO_STREAM("\n"
-                  << "AEB fault? " << unsigned(aeb_msg.aeb_fault) << "\n"
-                  << "AEB engaged? " << unsigned(aeb_msg.aeb_engaged) << "\n"
-                  << "AEB acceleration is " << aeb_msg.aeb_accel << "\n");
+    AEB_FAULT = aeb_msg.aeb_fault;
+    AEB_ENGAGED = aeb_msg.aeb_engaged;
+    AEB_ACCEL = aeb_msg.aeb_accel;
+    can_comms_msg.aeb_fault = aeb_msg.aeb_fault;
 }
 
 void MasterTask::lcc_output_msg_callback(const common::lcc_output_msg& lcc_msg)
 {
-    ROS_INFO_STREAM("\n"
-                  << "LCC fault? " << unsigned(lcc_msg.lcc_fault) << "\n"
-                  << "LCC steer is " << lcc_msg.lcc_steer << "\n");
+    LCC_FAULT = lcc_msg.lcc_fault;
+    LCC_STEER = lcc_msg.lcc_steer;
+    can_comms_msg.lcc_fault = lcc_msg.lcc_fault;
 }
 
 bool MasterTask::sensor_diagnostic_callback_CH2(common::sensor_diagnostic_flag_CH2::Request &req_CH2, common::sensor_diagnostic_flag_CH2::Response &res_CH2)
 {
-    ROS_INFO_STREAM("\n"
-                  << "Front radar fault? " << unsigned(req_CH2.front_radar) << "\n");
-
-    if (req_CH2.front_radar)    return true;
-    else    return false;
+    FRONT_RADAR = req_CH2.front_radar;
+    can_comms_msg.front_radar_fault = !(req_CH2.front_radar);
+    return true;
 }
 
 bool MasterTask::sensor_diagnostic_callback_CH3(common::sensor_diagnostic_flag_CH3::Request &req_CH3, common::sensor_diagnostic_flag_CH3::Response &res_CH3)
 {
-    ROS_INFO_STREAM("\n"
-                  << "Left radar fault? " << unsigned(req_CH3.left_corner_radar) << "\n"
-                  << "Right radar fault? " << unsigned(req_CH3.right_corner_radar) << "\n");
-    
-    if (req_CH3.left_corner_radar && req_CH3.right_corner_radar)    return true;
-    else    return false;
+    LEFT_CORNER_RADAR = req_CH3.left_corner_radar;
+    RIGHT_CORNER_RADAR = req_CH3.right_corner_radar;
+    can_comms_msg.left_radar_fault = !(req_CH3.left_corner_radar);
+    can_comms_msg.right_radar_fault = !(req_CH3.right_corner_radar);
+    return true;
 }
 
 bool MasterTask::sensor_diagnostic_callback_CH4(common::sensor_diagnostic_flag_CH4::Request &req_CH4, common::sensor_diagnostic_flag_CH4::Response &res_CH4)
 {
-    ROS_INFO_STREAM("\n"
-                  << "Mobileye fault? " << unsigned(req_CH4.mobileye) << "\n");
-    
-    if (req_CH4.mobileye)    return true;
-    else    return false;
+    MOBILEYE = req_CH4.mobileye;
+    can_comms_msg.mobileye_fault = !(req_CH4.mobileye);
+    return true;
 }
 common::can_comms_data_msg MasterTask::get_can_comms_msg() { return can_comms_msg; }
+
+
+void MasterTask::CAV_1_5() {
+    if (!MOBILEYE) {
+        can_comms_msg.acc_valid = 0;
+        can_comms_msg.aeb_valid = 0;
+        can_comms_msg.lcc_valid = 0;
+    }
+}
+
+void MasterTask::CAV_1_6() {
+    if (!FRONT_RADAR) {
+        can_comms_msg.acc_valid = 0;
+        can_comms_msg.aeb_valid = 0;
+    }
+}
+
+void MasterTask::ACC_1_1() {
+    //buffer_time = 0.2 s
+    if (!ACC_ACTIVATION || !ACC_ALLOWED || ACC_FAULT || !FRONT_RADAR || !LEFT_CORNER_RADAR || !RIGHT_CORNER_RADAR || !MOBILEYE)
+    {
+        // std::cout << "acc activation " << ACC_ACTIVATION << " acc allowed " << ACC_ALLOWED << " acc fault " << ACC_FAULT << " FR " << FRONT_RADAR << " LR " << LEFT_CORNER_RADAR << " RR " << RIGHT_CORNER_RADAR << " ME " << MOBILEYE << std::endl;
+        ros::WallTime curr = ros::WallTime::now();
+        if (initial_OFF_request) {
+            std::cout << "initial request" << std::endl;
+            begin1 = ros::WallTime::now();
+            initial_OFF_request = false;
+        }
+        else if ((curr.toSec() - begin1.toSec()) >= buffer_time) {
+            std::cout << "ACC_1.1 long_accel = 0" << std::endl;
+            can_comms_msg.long_accel = 0;
+        }
+    }
+    else    initial_OFF_request = true; 
+}
+
+void MasterTask::ACC_15() {
+    if (LCC_STEER > 30) {
+        can_comms_msg.acc_valid = 0;
+        std::cout << "ACC_15 acc_valid = 0" << std::endl;
+    }
+}
+
+void MasterTask::ACC_17() {
+    if (!ACC_ACTIVATION) {
+        ros::WallTime curr = ros::WallTime::now();
+        if (initial_OFF_request_2) {
+            begin3 = ros::WallTime::now();
+            initial_OFF_request_2 = false;
+        }
+        else if ((curr.toSec() - begin3.toSec()) >= 0.200) {
+            can_comms_msg.acc_valid = 0;
+            std::cout << "ACC_17 acc_valid = 0" << std::endl;
+        }
+    }
+    else    initial_OFF_request_2 = true;
+}
+
+void MasterTask::ACC_20() {
+    if (!ACC_ALLOWED || !ACC_ACTIVATION) {
+        can_comms_msg.acc_valid = 0;
+        std::cout << "ACC_20 acc_valid = 0" << std::endl;
+    }
+}
+
+void MasterTask::ACC_4() {
+    if (!AEB_ALLOWED || !AEB_ACTIVATION || AEB_FAULT) {
+        can_comms_msg.acc_valid = 0;
+        std::cout << "ACC_4 acc_valid = 0" << std::endl;
+    }
+}
+
+void MasterTask::AEB_13() {
+    if (AEB_ENGAGED) {
+        can_comms_msg.long_accel = AEB_ACCEL;
+        std::cout << "AEB_13 long_accel = " << AEB_ACCEL << std::endl;
+    }
+    else {
+        can_comms_msg.long_accel = ACC_ACCEL;
+        std::cout << "AEB_13 long_accel = " << ACC_ACCEL << std::endl;
+    }
+}
+
+void MasterTask::AEB_22() {
+    if (VEHICLE_SPEED < 15) {
+        can_comms_msg.aeb_valid = 0;
+        std::cout << "AEB_22 aeb_valid = 0" << std::endl;
+    }    
+}
+
+void MasterTask::AEB_24() {
+    if (AEB_ACTIVATION && AEB_ALLOWED) {
+        ros::WallTime curr = ros::WallTime::now();
+        if (initial_OFF_request_5) {
+            begin6 = ros::WallTime::now();
+            initial_OFF_request_5 = false;
+        }
+        else if ((curr.toSec() - begin6.toSec()) >= 0.500) {
+            if (can_comms_msg.aeb_valid) {
+                can_comms_msg.aeb_valid = 1;
+                std::cout << "AEB_24 aeb_valid = 1" << std::endl;
+            }            
+        }
+    }
+    else    initial_OFF_request_5 = true;
+}
+
+void MasterTask::AEB_26() {
+    if (!AEB_ALLOWED || !AEB_ACTIVATION) {
+        can_comms_msg.aeb_valid = 0;
+        std::cout << "AEB_26 aeb_valid = 0" << std::endl;
+    }  
+}
+
+void MasterTask::CAV_2_2() {
+    if (abs(AEB_ACCEL) > 4.9)   can_comms_msg.long_accel = -4.9;
+}
+
+void MasterTask::LCC_1() {
+    if (LCC_STEER > 30) {
+        can_comms_msg.lcc_steer = 30;
+    }
+    else if (LCC_STEER < -30) {
+        can_comms_msg.lcc_steer = -30;
+    }
+}
+
+void MasterTask::LCC_10() {
+    if (LCC_ACTIVATION && LCC_ALLOWED) {
+        ros::WallTime curr = ros::WallTime::now();
+        if (initial_OFF_request_6) {
+            begin7 = ros::WallTime::now();
+            initial_OFF_request_6 = false;
+        }
+        else if ((curr.toSec() - begin7.toSec()) >= 0.500) {
+            if (can_comms_msg.lcc_valid) {
+                can_comms_msg.lcc_valid = 1;
+                std::cout << "LCC_10 lcc_valid = 1" << std::endl;
+            }
+            
+        }
+    }
+    else    initial_OFF_request_6 = true;
+}
+
+void MasterTask::LCC_11() {
+    if (!LCC_ACTIVATION || !LCC_ALLOWED) {
+        ros::WallTime curr = ros::WallTime::now();
+        if (initial_OFF_request_7) {
+            begin8 = ros::WallTime::now();
+            initial_OFF_request_7 = false;
+        }
+        else if ((curr.toSec() - begin8.toSec()) >= 0.200) {
+            can_comms_msg.lcc_valid = 0;
+            std::cout << "LCC_11 lcc_valid = 0" << std::endl;
+        }
+    }
+    else    initial_OFF_request_7 = true;
+}
+
+void MasterTask::LCC_3() {
+    curr_vel = VEHICLE_SPEED;
+    ros::WallTime curr_time = ros::WallTime::now();
+    ego_accel = (curr_vel - prev_vel)/(curr_time.toSec() - prev_time.toSec());
+    if (abs(ego_accel) > 2.5 || abs(ACC_ACCEL) > 2.5 || abs(AEB_ACCEL) > 2.5) {
+        can_comms_msg.lcc_valid = 0;
+        std::cout << "LCC_3 lcc_valid = 0" << std::endl;
+    }
+    else
+    {
+        if (can_comms_msg.lcc_valid) {
+            can_comms_msg.lcc_valid = 1;
+            std::cout << "LCC_3 lcc_valid = 1" << std::endl;
+        }
+        
+    }
+    prev_time = curr_time;
+    prev_vel = curr_vel;
+}
