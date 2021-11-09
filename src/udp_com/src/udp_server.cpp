7ec46d86b6ce14f5d6c24d8a9b2704e76fcbdf28
Bahar Kholdi-Sabeti <43182069+baharkholdisabeti@users.noreply.github.com>
2021-11-09 13:39:37 -0500

    add udp server to listen to cohda (#95)

---
+++ src/udp_com/src/udp_server.cpp
@@ -0,0 +1,45 @@
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include "ros/ros.h"
+
+// Boost asio DAYTIME 5 example: https://www.boost.org/doc/libs/1_77_0/doc/html/boost_asio/tutorial/tutdaytime5.html
+
+using boost::asio::ip::udp;
+
+std::string make_daytime_string() {
+  using namespace std;  // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
+
+int main() {
+  try {
+    boost::asio::io_service io_service;
+
+    udp::socket socket(io_service, udp::endpoint(udp::v4(), 15000));
+
+    for (;;) {
+      boost::array<char, 1> recv_buf;
+      udp::endpoint remote_endpoint;
+      boost::system::error_code error;
+      socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint, 0, error);
+
+      if (error && error != boost::asio::error::message_size) {
+        throw boost::system::system_error(error);
+      }
+
+      std::string message = make_daytime_string();
+      std::cerr << message << std::endl;
+
+      boost::system::error_code ignored_error;
+      socket.send_to(boost::asio::buffer(message), remote_endpoint, 0, ignored_error);
+    }
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
